ab605050aca943acfa261025e90e851be9144b49
Jan Korbel (st69799) <71786085+duffi4545@users.noreply.github.com>
2023-03-06 22:48:34 +0100

    init

---
+++ src/main/java/cz/upce/fei/cv01/domain/AppUser.java
@@ -0,0 +1,60 @@
+package cz.upce.fei.cv01.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private Boolean active;
+
+    @Column
+    private LocalDateTime creationDate;
+
+    @Column
+    private LocalDateTime updateDate;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.REMOVE)
+    private List<Task> tasks= Collections.emptyList();
+
+    @ManyToMany(mappedBy = "users")
+    private List<Role> roles = Collections.emptyList();
+
+    public AppUser(String username, String password, Boolean active, LocalDateTime creationDate, LocalDateTime updateDate) {
+        this.username = username;
+        this.password = password;
+        this.active = active;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+    }
+
+    public AppUser(Long id, String username, String password, Boolean active, LocalDateTime creationDate, LocalDateTime updateDate) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.active = active;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+    }
+
+
+}
