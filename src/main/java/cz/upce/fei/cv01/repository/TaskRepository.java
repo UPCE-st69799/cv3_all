e4bdc76cf3688dd105f04173d433e3d5f4c653ce
Jan Korbel (st69799) <71786085+duffi4545@users.noreply.github.com>
2023-03-08 15:43:07 +0100

    5 fixed

--- src/main/java/cz/upce/fei/cv01/repository/TaskRepository.java
+++ src/main/java/cz/upce/fei/cv01/repository/TaskRepository.java
@@ -2,9 +2,11 @@ package cz.upce.fei.cv01.repository;
 
 import cz.upce.fei.cv01.domain.Task;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends PagingAndSortingRepository<Task, Long> {
-List<Task> findbyAuthorId(final Long authorId);
+List<Task> findAllByAuthorId(final Long authorId);
 }
--- src/main/java/cz/upce/fei/cv01/service/TaskService.java
+++ src/main/java/cz/upce/fei/cv01/service/TaskService.java
@@ -3,7 +3,6 @@ package cz.upce.fei.cv01.service;
 import cz.upce.fei.cv01.domain.Task;
 import cz.upce.fei.cv01.repository.TaskRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +13,6 @@ public class TaskService {
     private final TaskRepository taskRepository;
 
     public List<Task> findAllByAuthorId(final long authorId){
-        return taskRepository.findbyAuthorId(authorId);
+        return taskRepository.findAllByAuthorId(authorId);
     }
 }
